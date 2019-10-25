1b533f6b17b5eb6c0c7cb8ac974e9798cb1046d4
Sabique <sabiquekhan@gmail.com>
2019-10-26 00:54:58 +0530

    Setup the environment file configuration

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const mongodb = require('mongodb');
 
-let connectionString = 'mongodb+srv://todoAppUser:todoAppUserPWD@clusternodejs-bt3dh.mongodb.net/social-blogger?retryWrites=true&w=majority';
-
-mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
+mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
     module.exports = client.db();
     const app = require('./app');
     app.listen(process.env.PORT || 3000);
