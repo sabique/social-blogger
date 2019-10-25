6356b1e5e93dfcd67fdd2e7784b475bb3e86ee85
Sabique <sabiquekhan@gmail.com>
2019-10-25 19:18:56 +0530

    Implemented the model and validations for sign up form

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const router = require('./router')
 let port = process.env.PORT || 3000;
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.use(express.static('public'));
 app.set('views', 'views');
 app.set('view engine', 'ejs');
